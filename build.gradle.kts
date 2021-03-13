a1d3fa3a8b90dcaf49de4905e909dc9012caa2d2
Georgy Dzutsev <dzgeorgy@gmail.com>
2021-03-13 21:04:40 +0300

    Added 'data' module Extracted common build logic for android libs
    Signed-off-by: Georgy Dzutsev <dzgeorgy@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() //Necessary for kotlinx-collections-immutable-jvm from compose.ui:ui
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":domain"))
     implementation(Dependencies.Libraries.core)
     testImplementation(Dependencies.TestLibraries.junit)
     androidTestImplementation(Dependencies.TestLibraries.testJunit)
